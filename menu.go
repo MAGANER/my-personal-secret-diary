4636ecdc3067ab24ede176565905dd9505adb303
MAGANER <maganer.krik@mail.ru>
2021-02-06 22:01:05 +0300

    contains function to work with menu

--- menu.go
+++ menu.go
@@ -14,7 +14,8 @@ func print_help() {
 	fmt.Println("Commands:")
 	fmt.Println("help == see this")
 	fmt.Println("open == open existing diary")
-	fmt.Println("add  == add new page to diary")
+	fmt.Println("add  == add new page to existing diary")
+	fmt.Println("make == make new diary")
 }
 func get_data(file, question string) (string, string) {
 	reader := bufio.NewReader(os.Stdin)
@@ -25,6 +26,20 @@ func get_data(file, question string) (string, string) {
 	second, _ := reader.ReadString('\n')
 
 	return clear_str(path),clear_str(second)
+}
+func get_data_to_add(file, question, topic, page_path string) (string, string, string, string) {
+	reader := bufio.NewReader(os.Stdin)
+
+	fmt.Print(file)
+	path, _ := reader.ReadString('\n')
+	fmt.Print(question)
+	password, _ := reader.ReadString('\n')
+	fmt.Print(topic)
+	_topic, _ := reader.ReadString('\n')
+	fmt.Print(page_path)
+	page, _ := reader.ReadString('\n')
+
+	return clear_str(path),clear_str(password),clear_str(_topic), clear_str(page)
 }
 func run_menu() int {
 	reader := bufio.NewReader(os.Stdin)
@@ -42,6 +57,9 @@ func run_menu() int {
 		}
 		if val == "add" {
 			return 1
+		}
+		if val == "make" {
+			return 2
 		}
 
 		execute_cmd_command("cls")
