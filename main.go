813881e2500d0823269dc83058d4ca7c699034ad
MAGANER <maganer.krik@mail.ru>
2021-02-08 21:18:40 +0300

    fixed all. Create file and encrypt password. Add page to diary

--- main.go
+++ main.go
@@ -5,35 +5,33 @@ import "io/ioutil"
 import "os"
 import "time"
 
-//TODO: generate key
-var key = "LKHlhb899Y09olUi"
 
 func run() {
+	//TODO: generate key
+	var key = "LKHlhb899Y09olUi"
+	
 	result := run_menu()
 	switch {
 	case result == 0:
+		//open diary
 		path, password := get_data("enter diary path:","enter password:")
-		strings := get_file_data(path)
-		for n:= 0; n < len(strings); n++ {
-			fmt.Println(strings[n])
-		}
-		fmt.Println(password)
+		real_password  := get_diary_password(path,key)
+		fmt.Println(password,real_password)
+
 	case result == 1:
 		diary, password, topic, new_page := get_data_to_add("enter diary path:",
 															"enter password:",
 														    "enter page topic:",
 														    "enter path to page:")
-		strings := get_file_data(diary)
-		real_password := strings[0]
-		real_password = real_password[1:len(real_password)-1]
+		real_password := get_diary_password(diary,key)
 		if real_password != clear_str(password) {
 			fmt.Println("password is incorrect!", real_password)
 			time.Sleep(2*time.Second)
 			run()
 		}
 
-		new_strings := get_file_data(new_page)
-		add_to_file(diary, new_strings,topic,real_password)
+		new_strings := read_file_lines(new_page)
+		add_to_file(diary, new_strings,topic,key)
 		
 	case result == 2:
 		name, password := get_data("enter new diary name:","enter password for save data:")
